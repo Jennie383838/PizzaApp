d5c9739a1442ce91f995745764f26cab343e796b
Xavier Thong <xavierthong4@gmail.com>
2025-10-24 12:56:25 +0800

    Whatever message goes here

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Header component
+function Header() {
+    return <h1>Welcome to My Pizza Shop</h1>;
+}
+
+// Pizza component
+function Pizza({ name, toppings, image }) {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <img src={image} alt={name} style={{ width: "200px", height: "auto" }} />
+            <p>Toppings: {toppings.join(", ")}</p>
+        </div>
+    );
+}
+
+// Array of pizza data
+const pizzaData = [
+    { name: "Focaccia", toppings: ["Olive Oil", "Salt"], image: "/images/focaccia.jpg" },
+    { name: "Funghi", toppings: ["Mushroom", "Cheese"], image: "/images/funghi.jpg" },
+    { name: "Margherita", toppings: ["Cheese", "Tomato"], image:"images/margherita.jpg" },
+    { name: "Prosciutto", toppings: ["Ham", "Cheese"], image: "/images/prosciutto.jpg" },
+    { name: "Salamino", toppings: ["Salami", "Cheese"], image: "/images/salamino.jpg" },
+    { name: "Spinaci", toppings: ["Spinach", "Cheese"], image: "/images/spinaci.jpg" },
+];
+
+// App component
+function App() {
+    return (
+        <div>
+            <Header />
+            {pizzaData.map((pizza, index) => (
+                <Pizza
+                    key={index}
+                    name={pizza.name}
+                    toppings={pizza.toppings}
+                    image={pizza.image}
+                />
+            ))}
+        </div>
+    );
+}
+
+// Render App to the screen
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
